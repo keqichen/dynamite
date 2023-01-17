d5beabc635298cfaaad65e71ac360c3ee21aa56b
Keqi Chen <ckq009@gmail.com>
2023-01-17 15:42:44 +0000

    Add a clever bot

--- myBot.js
+++ myBot.js
@@ -1,7 +1,56 @@
+// const randomBot = require("./randomBot");
+
 class Bot {
     makeMove(gamestate) {
-        // Your code here
+        var counterForDynamiteP2=0;
+        var counterForDynamiteP1=0;
+
+        var counterForWaterP2=0;
+
+
+        for (let round of gamestate.rounds){
+            
+            if (round.p1==="D"){
+                counterForDynamiteP1++;
+            }
+
+            if (round.p2==="D"){
+                counterForDynamiteP2++;
+            }       
+
+            if (round.p2==="W"){
+                counterForWaterP2++;
+            }
+        }
+
+        if (gamestate.rounds.length < 100) {  
+            if (counterForWaterP2 > 10){
+                let randomIndex = Math.floor(3 * Math.random())
+                return ['R', 'P', 'S'][randomIndex]                
+            } else {
+                return "D";  
+            }
+         } else {
+
+                if (counterForDynamiteP1==100){
+                    let randomIndex = Math.floor(3 * Math.random())
+                        return ['R', 'P', 'S'][randomIndex] 
+                } else if (counterForDynamiteP1<100){
+                    return "D";
+                } {
+            
+                        if (counterForDynamiteP2==100){
+                            let randomIndex = Math.floor(3 * Math.random())
+                            return ['R', 'P', 'S'][randomIndex] 
+                        } else {
+                            let randomIndex = Math.floor(4 * Math.random())
+                            return ['R', 'P', 'S', 'W'][randomIndex]       
+                    }
+                }
+
+            }
+        }
     }
-}
+
 
 module.exports = new Bot();
