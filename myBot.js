2c2ebc576641f4fe042cd94ffd099ef8dc501160
Ying Cheng <yingyingch@yahoo.co.uk>
2023-01-17 16:12:40 +0000

    revised version

--- myBot.js
+++ myBot.js
@@ -2,55 +2,55 @@
 
 class Bot {
     makeMove(gamestate) {
-        var counterForDynamiteP2=0;
-        var counterForDynamiteP1=0;
+        var counterForDynamiteP2 = 0;
+        var counterForDynamiteP1 = 0;
 
-        var counterForWaterP2=0;
+        var counterForWaterP2 = 0;
 
 
-        for (let round of gamestate.rounds){
-            
-            if (round.p1==="D"){
+        for (let round of gamestate.rounds) {
+
+            if (round.p1 === "D") {
                 counterForDynamiteP1++;
             }
 
-            if (round.p2==="D"){
+            if (round.p2 === "D") {
                 counterForDynamiteP2++;
-            }       
+            }
 
-            if (round.p2==="W"){
+            if (round.p2 === "W") {
                 counterForWaterP2++;
             }
         }
 
-        if (gamestate.rounds.length < 100) {  
-            if (counterForWaterP2 > 10){
+        if (gamestate.rounds.length < 100) {
+            if (counterForWaterP2 > 10) {
                 let randomIndex = Math.floor(3 * Math.random())
-                return ['R', 'P', 'S'][randomIndex]                
+                return ['R', 'P', 'S'][randomIndex]
             } else {
-                return "D";  
+                return "D";
             }
-         } else {
+        } else {
 
-                if (counterForDynamiteP1==100){
+            if (counterForDynamiteP1 == 100) {
+                let randomIndex = Math.floor(3 * Math.random())
+                return ['R', 'P', 'S'][randomIndex]
+            } else if (counterForDynamiteP1 < 100) {
+                return "D";
+            } {
+                // need to improve this section. Now it's just random;
+                if (counterForDynamiteP2 == 100) {
                     let randomIndex = Math.floor(3 * Math.random())
-                        return ['R', 'P', 'S'][randomIndex] 
-                } else if (counterForDynamiteP1<100){
-                    return "D";
-                } {
-                        // need to improve this section. Now it's just random;
-                        if (counterForDynamiteP2==100){
-                            let randomIndex = Math.floor(3 * Math.random())
-                            return ['R', 'P', 'S'][randomIndex] 
-                        } else {
-                            let randomIndex = Math.floor(4 * Math.random())
-                            return ['R', 'P', 'S', 'W'][randomIndex]       
-                    }
+                    return ['R', 'P', 'S'][randomIndex]
+                } else {
+                    let randomIndex = Math.floor(4 * Math.random())
+                    return ['R', 'P', 'S', 'W'][randomIndex]
                 }
-
             }
+
         }
     }
+}
 
 
 module.exports = new Bot();
